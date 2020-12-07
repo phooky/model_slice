a77eb7fb7bffa625864345930d43c535cd05c8fd
phooky <phooky@gmail.com>
2020-12-07 10:18:21 -0500

    stl_io is too slow

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ extern crate clap;
 use clap::{Arg, App};
 use std::fs::File;
 
+enum RelPos {
+    ABOVE,
+    BELOW,
+    ON,
+}
+
 fn main() {
     let clap = App::new("Simple solid model slicer")
         .about("Cuts a model along a plane orthogonal to the z-axis")
@@ -18,8 +24,14 @@ fn main() {
     let z : f32 = matches.value_of("z-height").unwrap()
         .parse().unwrap();
     let path = matches.value_of("FILE").unwrap();
-    let f = File::open(path).unwrap();
+    let mut f = File::open(path).unwrap();
 
     println!("Loading model {}",path);
+    let stl = stl_io::create_stl_reader(&mut f).unwrap();
+    let mut tcount = 0;
+    for tri in stl {
+        tcount = tcount + 1;
+    }
+    println!("Triangle count {}",tcount);
     println!("Slicing model at z-height {}",z);
 }
