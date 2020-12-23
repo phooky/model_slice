bc122208eabb2de28f1777deccf16f55c3d43f6f
phooky <phooky@gmail.com>
2020-12-22 21:43:54 -0500

    noping out on hashing floats, just not going to go there

--- src/build_face.rs
+++ src/build_face.rs
@@ -1,7 +1,11 @@
 use std::hash::{Hash, Hasher};
+use std::cmp::{Eq, PartialEq};
+use std::collections::HashMap;
+
 use stl_io::Vertex;
 
 // I much prefer x/y to array indexing
+#[derive(PartialEq, Eq)]
 struct Vertex2 { x : f32, y : f32 }
 
 // A segment is a _directed_ pair of 2d vertices
@@ -21,3 +25,24 @@ impl Segment {
             b : Vertex2 { x: v2[0], y : v2[1] }, }
     }
 }
+
+/*
+// We're going to attempt the hash table approach for now.
+pub fn build_loops(segs : &Vec<Segment>) -> Vec<Vec<Vertex2> > {
+    let mut loops = Vec::new();
+    let mut hashtab = HashMap::new();
+    for seg in segs {
+        if !hashtab.contains_key(seg.a) {
+            hashtab.insert(seg.a,Vec::new());
+        }
+        if !hashtab.contains_key(seg.b) {
+            hashtab.insert(seg.b,Vec::new());
+        }
+        hashtab.get_mut(seg.b).unwrap().push(seg.a);
+        hashtab.get_mut(seg.a).unwrap().push(seg.b);
+    }
+    loops
+}*/
+
+// NOPEING OUT on hashing floats. This is a bullshit idea.
+
