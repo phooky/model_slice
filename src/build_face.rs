99aa29619499a02f89a3a22490acd9d2aaacaf0f
phooky <phooky@gmail.com>
2020-12-24 22:17:00 -0500

    starting in with kd tree but probably will move to rtree

--- src/build_face.rs
+++ src/build_face.rs
@@ -1,22 +1,12 @@
-use std::hash::{Hash, Hasher};
-use std::cmp::{Eq, PartialEq};
-use std::collections::HashMap;
-
+use kd_tree::{KdPoint, KdTree};
 use stl_io::Vertex;
+extern crate typenum;
 
 // I much prefer x/y to array indexing
-#[derive(PartialEq, Eq)]
 struct Vertex2 { x : f32, y : f32 }
 
 // A segment is a _directed_ pair of 2d vertices
 pub struct Segment { a : Vertex2, b: Vertex2 }
-
-impl Hash for Vertex2 {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.x.to_bits().hash(state);
-        self.y.to_bits().hash(state);
-    }
-}
 
 impl Segment {
     pub fn new(v1 : &Vertex, v2 : &Vertex) -> Segment {
@@ -26,8 +16,28 @@ impl Segment {
     }
 }
 
+pub struct Loop {
+    pts : Vec<Vertex2>,
+    closed : bool,
+}
 
-// Strategy:
+struct TreeItem {
+    v : Vertex2,
+    idx : usize,
+}
+
+impl KdPoint for TreeItem {
+    type Scalar = f32;
+    type Dim = typenum::U2; 
+    fn at(&self, k: usize) -> f32 { if k == 0 { self.v.x } else {self.v.y} }
+}
+
+pub fn build_loops(segs : &Vec<Segment>) -> Vec<Loop> {
+    let mut loops = Vec::new();
+    loops
+}
+
+// Alternate Strategy:
 // throw every segment in a K-D tree twice. Key is vertex. Value
 // is a list of vertices or chains. (Should everything start as a
 // 1-length chain? It seems clever, but may involve more data 
@@ -39,9 +49,4 @@ impl Segment {
 // to chains (although position annotations would suck), but then we've
 // got a separate split chain issue, etc. Doing all insertions first and
 // keeping track of X points separately may do the trick.
-//
-// So, the value of an entry would be:
-// enum { Vertex2, (Vertex2, Vertex2), pointer to longer list (Xpt) }
-// First variant is incomplete, second ideal, third complicated.
-//
 
